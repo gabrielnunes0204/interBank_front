e09265b7c853236ea9eec69f7283cf687574e39f
gabrielnunes0204 <gabriel.microsmp@gmail.com>
2025-01-30 23:50:58 -0300

    add paths home and visualizacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { VisualizarAgendaComponent } from './visualizar-agenda/visualizar-agenda.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {component: HomeComponent, path: ""},
+    {component: VisualizarAgendaComponent, path: "visualizar"}
+];
